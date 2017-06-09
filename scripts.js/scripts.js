62f8dbc0ab1fde9d573a39cf1ac85b2f189a99d9
Smallz.tm <markkariuki17@gmail.com>
2017-06-09 17:27:14 +0300

    made the ping pong web app

---
+++ scripts.js/scripts.js
@@ -0,0 +1,22 @@
+//bui
+function ping-pong(user input){
+
+}
+var result = [];
+
+for (var index = 1; index <= userinput; index++){
+  if((index % 15) === 0){
+    result.push  (*ping-pong*);
+  } else if ((index % 5) === 0)
+    result.push ("pong");
+  } else if ((index % 3) === 0){
+    result.push ("ping");
+  } else
+    result.push (index);
+  }
+    return result;
+
+  }
+  //user-interface
+  $(document).ready(function() {
+    $("")
